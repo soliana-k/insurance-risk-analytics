6e089eca2fe8b0b0d16071b3c0d92c9a389bba9b
kalkidan <kalukassahun29@gmail.com>
2026-05-20 23:48:08 +0300

    feat: initial insurance eda setup with class encapsulation

---
+++ src/insurance_EDA.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import logging
+import os 
+
+class ImsuranceEda:
+    def __init__(self, df):
+        self.df=df
+        pass
+
+    def descriptive_stats(self):
+        pass
+
+
+    def data_quality(self):
+        pass
+
+
+    def univaraite_analysis(self):
+        pass
+
+    def multi(self):
+        pass
